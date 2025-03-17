33eb7a4dc6e4dffc21fcbb575353fa45077744cc
Serge Kovbasiuk <isserge@protonmail.com>
2025-03-17 11:43:09 +0700

    add config unit tests

--- src/config.rs
+++ src/config.rs
@@ -1,6 +1,10 @@
 use anyhow::{Context, Result};
 use std::env;
 
+const DEFAULT_DATABASE_URL: &str = "sqlite://data/data.db";
+const DEFAULT_GITHUB_GRAPHQL_URL: &str = "https://api.github.com/graphql";
+const DEFAULT_POLL_INTERVAL: u64 = 10;
+
 #[derive(Debug)]
 pub struct Config {
     pub github_token: String,
@@ -16,15 +20,124 @@ impl Config {
             github_token: env::var("GITHUB_TOKEN")
                 .context("GITHUB_TOKEN environment variable is required")?,
             github_graphql_url: env::var("GITHUB_GRAPHQL_URL")
-                .unwrap_or_else(|_| "https://api.github.com/graphql".to_string()),
+                .unwrap_or_else(|_| DEFAULT_GITHUB_GRAPHQL_URL.to_string()),
             telegram_bot_token: env::var("TELOXIDE_TOKEN")
                 .context("TELOXIDE_TOKEN environment variable is required")?,
             poll_interval: env::var("POLL_INTERVAL")
                 .ok()
                 .and_then(|v| v.parse().ok())
-                .unwrap_or(10),
+                .unwrap_or(DEFAULT_POLL_INTERVAL),
             database_url: env::var("DATABASE_URL")
-                .unwrap_or_else(|_| "sqlite://data/data.db".to_string()),
+                .unwrap_or_else(|_| DEFAULT_DATABASE_URL.to_string()),
         })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use temp_env::with_vars;
+
+    #[test]
+    fn test_from_env() {
+        with_vars(
+            [
+                ("GITHUB_TOKEN", Some("test github token")),
+                ("GITHUB_GRAPHQL_URL", Some("https://api.github.com/graphql")),
+                ("TELOXIDE_TOKEN", Some("test telegram bot token")),
+                ("POLL_INTERVAL", Some("100")),
+                ("DATABASE_URL", Some("sqlite://test/test.db")),
+            ],
+            || {
+                let config = Config::from_env().unwrap();
+                assert_eq!(config.github_token, "test github token");
+                assert_eq!(config.github_graphql_url, "https://api.github.com/graphql");
+                assert_eq!(config.telegram_bot_token, "test telegram bot token");
+                assert_eq!(config.poll_interval, 100);
+                assert_eq!(config.database_url, "sqlite://test/test.db");
+            },
+        );
+    }
+
+    #[test]
+    fn test_missing_github_token_error() {
+        with_vars(
+            [
+                ("GITHUB_GRAPHQL_URL", Some("https://api.github.com/graphql")),
+                ("TELOXIDE_TOKEN", Some("test telegram bot token")),
+                ("POLL_INTERVAL", Some("100")),
+                ("DATABASE_URL", Some("sqlite://test/test.db")),
+            ],
+            || {
+                unsafe {
+                    env::remove_var("GITHUB_TOKEN");
+                }
+                let config = Config::from_env();
+                assert!(config.is_err());
+            },
+        );
+    }
+
+    #[test]
+    fn test_missing_telegram_bot_token_error() {
+        with_vars(
+            [
+                ("GITHUB_GRAPHQL_URL", Some("https://api.github.com/graphql")),
+                ("GITHUB_TOKEN", Some("test github token")),
+                ("POLL_INTERVAL", Some("100")),
+                ("DATABASE_URL", Some("sqlite://test/test.db")),
+            ],
+            || {
+                let config = Config::from_env();
+                assert!(config.is_err());
+            },
+        );
+    }
+
+    #[test]
+    fn test_missing_database_url_default() {
+        with_vars(
+            [
+                ("GITHUB_GRAPHQL_URL", Some("https://api.github.com/graphql")),
+                ("GITHUB_TOKEN", Some("test github token")),
+                ("TELOXIDE_TOKEN", Some("test telegram bot token")),
+                ("POLL_INTERVAL", Some("100")),
+            ],
+            || {
+                let config = Config::from_env().unwrap();
+
+                assert_eq!(config.database_url, DEFAULT_DATABASE_URL);
+            },
+        );
+    }
+
+    #[test]
+    fn test_missing_github_graphql_url_default() {
+        with_vars(
+            [
+                ("GITHUB_TOKEN", Some("test github token")),
+                ("TELOXIDE_TOKEN", Some("test telegram bot token")),
+                ("POLL_INTERVAL", Some("100")),
+            ],
+            || {
+                let config = Config::from_env().unwrap();
+                assert_eq!(config.github_graphql_url, DEFAULT_GITHUB_GRAPHQL_URL);
+            },
+        );
+    }
+
+    #[test]
+    fn test_missing_poll_interval_default() {
+        with_vars(
+            [
+                ("GITHUB_TOKEN", Some("test github token")),
+                ("GITHUB_GRAPHQL_URL", Some("https://api.github.com/graphql")),
+                ("TELOXIDE_TOKEN", Some("test telegram bot token")),
+            ],
+            || {
+                let config = Config::from_env().unwrap();
+                assert_eq!(config.poll_interval, DEFAULT_POLL_INTERVAL);
+            },
+        );
     }
 }
